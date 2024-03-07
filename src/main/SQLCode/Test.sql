7b0e2f7b1316a34b3225354c41687246fda1b85d
Rahul-Sharma-05 <rahulkumar219209200@gmail.com>
2024-03-07 14:25:36 +0530

    updating SQL file

--- src/main/SQLCode/Test.sql
+++ src/main/SQLCode/Test.sql
@@ -1,12 +1,11 @@
 CREATE database test;
 USE test;
-
+use learnhiber;
 
 Create table Admin (
 AdminId int primary key,
 Password varchar(10)
 );
-
 
 Insert into Admin values
 (10, "Admin@123"),
@@ -18,6 +17,19 @@ Insert into Admin values
 Select * from Admin;
 
 
+Create table Admin (
+AdminId varchar(20) primary key,
+Password varchar(10)
+);
+
+Insert into Admin values
+("10", "Admin@123"),
+("112", "Admin@1555"),
+("12", "Admin@1555"),
+("115", "Admin@88"),
+("1885", "Admin@998");
+
+Select * from Admin;
 
 CREATE TABLE Book
 (BookId INT auto_increment PRIMARY KEY ,
@@ -27,14 +39,15 @@ BookPublication varchar(50),
 BookQuantity INT default 0
 );
 
-
-INSERT INTO Book (BookName, BookAuthor, BookPublication)
+INSERT INTO Book (BookName, BookAuthor, BookPublication, BookQuantity)
 VALUES 
 ("Fire of Wings", "A.P.J. Kalam",	"KD Publication", 10),
 ("My Experiment with Truth", "M.K. Gandhi",	"RK Books", 15),
 ("XYZ",	"Ram Rahim", "Kiran Publication", 5),
 ("Programming with C", "Reema Thareja", "OxFord Publication", 8),
-("Programming with C", "Reema Thareja", "OxFord Publication"),
-("Programming with C", "Reema Thareja", "OxFord Publication");
+("Programming with C", "Reema Thareja", "OxFord Publication", 0),
+("Programming with C", "Reema Thareja", "OxFord Publication", 0);
+
+
 
 SELECT * FROM Book;
