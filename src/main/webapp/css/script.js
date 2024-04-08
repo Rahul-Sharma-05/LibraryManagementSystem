ea4c239b01fc75222d60798738fbc52fa22e2209
Rahul Sharma <rahulkumar219209200@gmail.com>
2024-04-08 17:47:06 +0530

    HomePage Changed

---
+++ src/main/webapp/css/script.js
@@ -0,0 +1,62 @@
+let $slides, interval, $selectors, $btns, currentIndex, nextIndex;
+
+let cycle = index => {
+  let $currentSlide, $nextSlide, $currentSelector, $nextSelector;
+
+  nextIndex = index !== undefined ? index : nextIndex;
+
+  $currentSlide = $($slides.get(currentIndex));
+  $currentSelector = $($selectors.get(currentIndex));
+
+  $nextSlide = $($slides.get(nextIndex));
+  $nextSelector = $($selectors.get(nextIndex));
+
+  $currentSlide.removeClass("active").css("z-index", "0");
+
+  $nextSlide.addClass("active").css("z-index", "1");
+
+  $currentSelector.removeClass("current");
+  $nextSelector.addClass("current");
+
+  currentIndex = index !== undefined ?
+  nextIndex :
+  currentIndex < $slides.length - 1 ?
+  currentIndex + 1 :
+  0;
+
+  nextIndex = currentIndex + 1 < $slides.length ? currentIndex + 1 : 0;
+};
+
+$(() => {
+  currentIndex = 0;
+  nextIndex = 1;
+
+  $slides = $(".slide");
+  $selectors = $(".selector");
+  $btns = $(".btn");
+
+  $slides.first().addClass("active");
+  $selectors.first().addClass("current");
+
+  interval = window.setInterval(cycle, 6000);
+
+  $selectors.on("click", e => {
+    let target = $selectors.index(e.target);
+    if (target !== currentIndex) {
+      window.clearInterval(interval);
+      cycle(target);
+      interval = window.setInterval(cycle, 6000);
+    }
+  });
+
+  $btns.on("click", e => {
+    window.clearInterval(interval);
+    if ($(e.target).hasClass("prev")) {
+      let target = currentIndex > 0 ? currentIndex - 1 : $slides.length - 1;
+      cycle(target);
+    } else if ($(e.target).hasClass("next")) {
+      cycle();
+    }
+    interval = window.setInterval(cycle, 6000);
+  });
+});
